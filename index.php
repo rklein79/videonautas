f42437202800ac59d159ce70a23a2b94c493a505
Marcio Carvalho <marcio@cmfmidia.com>
2014-04-03 12:32:25 -0300

    montando esqueleto header index

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<script type="text/javascript" src="js/index.js"></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js" type="text/javascript"></script>
+	<title>VN Esqueleto</title>
+</head>
+<body>
+    <div id="wrap">
+            <div id="header">
+                <div id="top-menu">
+                    <ul>
+                        <a href=""><li>Quem Somos</li></a>
+                        <a href=""><li>Apresentadores</li></a>
+                        <a href=""><li>Suporte</li></a>
+                        <a href=""><li>Contatos</li></a>
+                    </ul>
+                    <a href=""><span><img id="entrar-img" src="images/key.png" width="21px"/><div id="entrar-texto">Entrar</div></span></a>
+                </div>
+                <div id="main-header">
+                    <div id="logo"></div>
+                </div>
+                <div id="search">
+                        <input type="text" id="search-field">
+                        <span id="search-button"></span>
+                </div>
+                <div id="bottom-menu">
+                    <ul>
+                        <a href=""><li>Plug-In Play</li></a>
+                        <a href=""><li>Especialista</li></a>
+                        <a href=""><li>Categorias</li></a>
+                    </ul>
+                    <span id="social-header">
+                        <a href="" alt="Instagram" class="social"><img src="images/Instagram.png"/></a>
+                        <a href="" alt="Facebook" class="social"><img src="images/facebook.png"/></a>
+                        <a href="" alt="Twitter" class="social"><img src="images/twitter.png"/></a>
+                        <a href="" alt="Youtube" class="social"><img src="images/youtube.png"/></a>
+                    </span>
+                </div>
+            </div>
+
+    </div>
+
+</body>
+
+ 
